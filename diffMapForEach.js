927cdcf864d76e9dddb99a19d9727db6614422fe
Sukhdevsingh123 <sukhdev.2100398@sanskriti.ac.in>
2023-12-02 07:37:13 -0800

    forEach

---
+++ diffMapForEach.js
@@ -0,0 +1,21 @@
+// map:- map return a newarray
+// forEach:- not return a new array 
+
+
+        //map
+const arr=[1,2,3,4,5];
+const data=arr.map(element => {
+  return element*2
+    
+});
+console.log(data)
+
+        //  reduce
+
+const array=[1,2,3,4,5];
+arr.forEach(element => {
+    
+    console.log(element*2);
+      
+  });
+  
---
+++ forEach.js
@@ -0,0 +1,6 @@
+const name=["taj","rohan","gautam","rahul","rohan","akhil","ram","deepak"];
+name.forEach((element,idx,arr) => {
+    console.log('element',element+1)
+    console.log('index',idx)
+    console.log('arr',arr)
+});
